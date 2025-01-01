1ed4450c2789e473da00c907eeea79c0a1c906a1
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2025-01-01 23:51:15 +0530

    working home screen of notes app

--- src/Screen/HomeScreen.tsx
+++ src/Screen/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, StatusBar, TouchableOpacity} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RootNavigationParaList} from '../Navigation/MainStack';
@@ -8,20 +8,84 @@ type HomeScreenProps = {
   navigation: StackNavigationProp<RootNavigationParaList, 'Home'>;
 };
 
+type noteType = {
+  "_id": string,
+  "title": string,
+  "description": string,
+  "postedById": string,
+  "postedByName": string,
+  "createdAt": string,
+  "updatedAt": string,
+  "__v": number
+}
+
 export default function HomeScreen({navigation}: HomeScreenProps) {
   const [details, setDetais] = useState<string>('');
+  const [notes, setNotes] = useState<noteType[]>([])
   useEffect(() => {
-    logindetails();
+    getAllNotes();
   }, []);
-  const logindetails = async () => {
-    const val = await AsyncStorage.getItem('User');
-    console.log('val :- ', val);
-    setDetais(val == null ? '' : val);
-  };
+  const getAllNotes = async() =>{
+    const user = await AsyncStorage.getItem('User');
+    if(user){
+      const parsedUser = JSON.parse(user); 
+      const userId = parsedUser._id;
+      
+    }
+  }
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Text>{details}</Text>
+    <View style={styles.container}>
+      <StatusBar backgroundColor={'white'} barStyle={'dark-content'}/>
+      <View style={styles.header}>
+        <Text style={styles.title}>
+          Notes App
+        </Text>
+      </View>
+      {
+
+      }
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnText}>
+          Create Note
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"white",
+  },
+  header:{
+    width:"100%",
+    height:60,
+    backgroundColor:"white",
+    elevation: 5,
+    justifyContent:"center",
+    paddingLeft: 20,
+    // alignItems:"center",
+  },
+  title:{
+    color:'black',
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  btn:{
+    position:'absolute',
+    width: 200,
+    height: 50,
+    borderRadius: 30,
+    right: 20,
+    bottom: 20,
+    backgroundColor:"black",
+    justifyContent:'center',
+    alignItems: 'center',
+  },
+  btnText:{
+    color:'white',
+    fontSize:16,
+    fontWeight:'600'
+  }
+})
---
+++ src/component/Loader.tsx
@@ -0,0 +1,34 @@
+import {ActivityIndicator, Modal, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+type LoaderProps = {
+  visible: boolean;
+};
+
+export default function Loader({visible}: LoaderProps) {
+  return (
+    <Modal transparent visible={visible}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'rgba(0,0,0, 0.5)',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <View
+          style={{
+            height: 80,
+            width: 80,
+            borderRadius: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'white',
+          }}>
+          <ActivityIndicator size={'large'} color={'black'} />
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({});

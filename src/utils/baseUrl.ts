066a146df614c649cf82ed31ac5d4eca4c4b3bca
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2025-01-26 14:22:17 +0530

    Now we can deploy your notes app in play store

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://192.168.31.199:8000/'; // please correct ip address for the api as both devices must be on same wifi -- http://localhost:8000/api/auth/login
+export const baseUrl = 'https://notesappbackend-kz8v.onrender.com/'; // please correct ip address for the api as both devices must be on same wifi -- http://localhost:8000/api/auth/login
 // 192.168.31.199
